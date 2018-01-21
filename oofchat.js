a1dd9454e6e47e47a2db1e3332ad3b5f1efacc08
ronthecookie <ronthecookie0101@gmail.com>
2018-01-21 15:01:43 +0200

    client and some server logic

--- oofchat.js
+++ oofchat.js
@@ -8,6 +8,14 @@ http.listen(port, () => {
 });
 io.on('connection', async socket => {
     console.log("a user connected from somewhere in the universe.");
-    
+    setInterval(() => {
+        if (socket.nickname) return;
+        socket.disconnect(true); // He was idle for more then 5 min without choosing a nickname! 
+        console.log("Kicked a user for idleing before choosing a nickname");
+    },5 * 60 * 1000);
+    socket.on('nickname', nickname => {
+        if (typeof nickname != "string") socket.disconnect(true); // huehue
+        socket.nickName = nickname;
+    });
 });
 app.use(express.static("static"));
