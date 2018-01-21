b20b09b02391aff826c501ed7386183ee6143d93
ronthecookie <ronthecookie0101@gmail.com>
2018-01-21 15:25:31 +0200

    lots of backend

--- static/client.js
+++ static/client.js
@@ -1,1 +1,18 @@
 var socket = io();
+/**
+ * @type {String}
+ */
+var nickname;
+/**
+ * Set self nickname, can only be used once per session.
+ * @param {String} nick 
+ */
+function setNickname(nick) {
+    if (nickname) throw new Error("Nickname can only be set once!");
+    socket.emit("nickname", nick);
+    nickname = nick
+}
+socket.on('disconnect', () => {
+    document.write("You have been kicked.");
+});
+setNickname("Dank af Memer");
