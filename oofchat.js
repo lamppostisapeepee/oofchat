3158fefdd0b537e8e8fa4a015018554cf4b69f09
ronthecookie <ronthecookie0101@gmail.com>
2018-01-22 17:38:09 +0200

    ahh

--- oofchat.js
+++ oofchat.js
@@ -79,7 +79,11 @@ io.on('connection', async socket => {
             /**
              * @type {String}
              */
-            let content = marked(escape(msg).split(new RegExp(/:[a-z-_]+:/giu)).map(v => emoji.lib[v.slice(1, v.length-2)] ?emoji.lib[v.slice(1, v.length-2)].char:v ).join(""));
+            let content = marked(escape(msg).split(" ").map(v => {
+                let em = emoji.lib[v.slice(1, v.length-2)]
+                if (v[0] != ":" || v[v.length-2] != ":" || !em) return v;
+                return em.char;
+            }).join(""));
             content = content.slice(3, content.length-5);
             io.emit("chat message", {author: socket.nickname, content, contentNoMarkdown: escape(msg)});
             console.log(`[CHAT] ${socket.nickname}: ${content}`);
